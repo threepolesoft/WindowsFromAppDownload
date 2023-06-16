260cc669024cc7d97cffe211b95b96e64caced22
threepolesoft <threepolesoft@gmail.com>
2023-06-16 22:35:31 +0600

    init

---
+++ WindowsFromAppDownload/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFromAppDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.pbDownload = new System.Windows.Forms.ProgressBar();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(506, 74);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(76, 20);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(119, 74);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(381, 20);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // pbDownload
+            // 
+            this.pbDownload.Location = new System.Drawing.Point(119, 111);
+            this.pbDownload.Name = "pbDownload";
+            this.pbDownload.Size = new System.Drawing.Size(381, 23);
+            this.pbDownload.TabIndex = 2;
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(116, 137);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(87, 13);
+            this.lbStatus.TabIndex = 3;
+            this.lbStatus.Text = "Downloaded 0 %";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 351);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.pbDownload);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Download";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.ProgressBar pbDownload;
+        private System.Windows.Forms.Label lbStatus;
+    }
+}
+
---
+++ WindowsFromAppDownload/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFromAppDownload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        WebClient webClient;
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(this.txtUrl.Text))
+                {
+
+                    pbDownload.Value = 0;
+                    lbStatus.Text = "Downloaded 0%";
+                    lbStatus.Update();
+
+                    Thread thread = new Thread(() =>
+                    {
+                        Uri uri = new Uri(this.txtUrl.Text);
+
+                        string fileName = System.IO.Path.GetFileName(uri.AbsolutePath);
+                        webClient.DownloadFileAsync(uri, Application.StartupPath + "/mm/" + fileName);
+                    });
+
+                    thread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webClient = new WebClient();
+            webClient.DownloadProgressChanged += webClient_DownloadProgressChanged;
+            webClient.DownloadFileCompleted += webClient_DownloadFileCompleted;
+        }
+
+        void webClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Downloaded complete!");
+        }
+
+        void webClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate(){
+                pbDownload.Minimum = 0;
+                double receive = double.Parse(e.BytesReceived.ToString());
+                double total = double.Parse(e.TotalBytesToReceive.ToString());
+
+                double persentage = receive / total * 100;
+
+                pbDownload.Value = int.Parse(Math.Truncate(persentage).ToString());
+
+                lbStatus.Text = "Downloaded " + persentage.ToString() + " %";
+                lbStatus.Update();
+
+            }));
+        }
+
+    }
+}
